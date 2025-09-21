678bd75e9dc056b16584a37f8356e0acc0e2352b
Jon Alc <jon.f.alcantara@gmail.com>
2025-09-21 03:35:38 -0700

    Create homepage

---
+++ app.py
@@ -0,0 +1,14 @@
+# Imports
+from flask import Flask
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+# My App
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Testing 123"
+
+if __name__ in "__main__":
+    app.run()
